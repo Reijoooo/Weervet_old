5b1b0112650e5e497c0999c5cba8d573a1c632c8
Reijoooo <griha200142@gmail.com>
2024-04-07 14:19:32 +0700

    1

--- about_app/views.py
+++ about_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    # Получение данных из базы данных или других источников
-    my_data = {'foo': 'bar', 'baz': 'qux'}
+
+def about(request):
 
     # Отправка данных в шаблон и возврат ответа
-    return render(request, 'index.html', {'my_data': my_data})
+    return render(request, 'about.html')
